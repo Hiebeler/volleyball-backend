f6fc95b533faa2b3c7ad7b22a8d6541a5e9b80c2
Hiebeler <emanuel.hiebeler@gmail.com>
2025-07-29 23:56:31 +0200

    return points in table

--- src/dtos/team.dto.ts
+++ src/dtos/team.dto.ts
@@ -1,9 +1,11 @@
 export class TeamDto {
   id: number;
   name: string;
+  points?: number;
 
   constructor(team: any) {
     this.id = team.id;
     this.name = team.name;
+    this.points = team.points;
   }
 }
--- src/services/table.service.ts
+++ src/services/table.service.ts
@@ -11,12 +11,40 @@ export class TableService {
     const tables = await this.prisma.table.findMany({
       include: {
         teams: true,
+        games: true,
       },
     });
     if (!tables) {
       throw new ApiError('no Tables found', 404);
     }
-    const tablesDto = tables.map((table) => new TableDto(table));
-    return tablesDto;
+    const tablesDtos: TableDto[] = [];
+    tables.forEach((table) => {
+      let tableDto = new TableDto(table);
+      tableDto = this.calculateTeamPoints(tableDto, table.games);
+      tablesDtos.push(tableDto);
+    });
+    return tablesDtos;
+  }
+
+  calculateTeamPoints(tableDto: TableDto, games: any[]): TableDto {
+    if (tableDto.teams) {
+      tableDto.teams = tableDto.teams.map((team) => {
+        let points = 0;
+        const gamesOfTeam = games.filter(
+          (game) =>
+            (game.team1Id === team.id || game.team2Id === team.id) && game.status === 'FINISHED',
+        );
+        gamesOfTeam.map((game) => {
+          const thisTeamScore = game.team1Id === team.id ? game.team1Score : game.team2Score;
+          const otherTeamScore = game.team1Id === team.id ? game.team2Score : game.team1Score;
+
+          if (thisTeamScore > otherTeamScore) points += 3;
+          else if (thisTeamScore === otherTeamScore) points += 1;
+        });
+        team.points = points;
+        return team;
+      });
+    }
+    return tableDto;
   }
 }
